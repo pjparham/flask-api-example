8e43246222a096337e5eebc048064851df7c8954
Parker Parham <parkerparham@gmail.com>
2023-05-22 19:40:07 -0400

    Adds get resource for Books

--- server/app.py
+++ server/app.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Resource, Api
 
+from models import db, Book
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -11,5 +13,12 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
+
+class Books(Resource):
+    def get(self):
+        books = [book.to_dict() for book in Book.query.all()]
+        return make_response(jsonify(books), 200)
+    
+api.add_resource(Books, '/books')
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 
+
 db = SQLAlchemy()
 
 class Book(db.Model, SerializerMixin):
@@ -11,7 +12,7 @@ class Book(db.Model, SerializerMixin):
     author = db.Column(db.String)
     page_count = db.Column(db.Integer)
     summary = db.Column(db.String)
-    categoy = db.Column(db.String)
+    category = db.Column(db.String)
 
     def __repr__(self):
         return f'<Book {self.title} by {self.author} | {self.category} | {self.page_count} pages>'
